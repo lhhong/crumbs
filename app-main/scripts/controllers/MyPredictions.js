11c41a809f112ca7c2d8401de0892ea2127b6c43
signapoop <leowyaoyang@gmail.com>
2017-03-13 19:05:51 +0800

    Fixed graph for view predictions

--- app-main/scripts/controllers/MyPredictions.js
+++ app-main/scripts/controllers/MyPredictions.js
@@ -137,4 +137,28 @@ angular.module('sbAdminApp')
 	    }
     };
 
+    $('#PredictionModal').on('shown.bs.modal', function (event) {
+
+        var button = $(event.relatedTarget);
+        var modal = $(this);
+        var canvas = modal.find('.modal-body canvas');
+
+        // Chart initialiser
+        var ctx = canvas[0].getContext("2d");
+        var chart = new Chart(ctx).Line({
+            labels: ["January", "February", "March", "April", "May"],
+            datasets: [
+                {
+                    fillColor: "rgba(190,144,212,0.2)",
+                    strokeColor: "rgba(190,144,212,1)",
+                    pointColor: "rgba(190,144,212,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56]
+                },
+            ]
+        }, {});
+    });
+
   }]);
